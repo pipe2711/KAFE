2e93221f5cc1d7a45fa2c164837b9ea6a31f0bf8
WatashiWaraio <karen.castaneda02@usa.edu.co>
2025-05-11 01:53:16 +0000

    Actualizacion server

--- KAFE-SAAS/SERVER-FLASK/main.py
+++ KAFE-SAAS/SERVER-FLASK/main.py
@@ -1,41 +1,34 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-import subprocess
 import tempfile
+import subprocess
 import os
 
-
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*"}}) #POR SEGURIDAD SE DEBE CAMBIAR 
+CORS(app, resources={r"/*": {"origins": "http://149.130.179.251:5173"}})
 
 @app.route('/ejecutar', methods=['POST'])
 def ejecutar_codigo():
     data = request.get_json()
-
     filename = data.get('filename', 'programa.kf')
     code = data.get('code', '')
- 
 
     if not code:
         return jsonify({'error': 'No hay código para ejecutar.'}), 400
 
     try:
-
         with tempfile.NamedTemporaryFile(delete=False, suffix='.kf', mode='w') as temp_file:
             temp_file.write(code)
             temp_filename = temp_file.name
 
-        
         result = subprocess.run(
-          ['python3', '/home/qwerty/Documents/Lenguajes/KAFE/src/Kafe.py', temp_filename],
+            ['python3', '/home/ubuntu/KAFE/src/Kafe.py', temp_filename],
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
             text=True
         )
 
         output = result.stdout + result.stderr
-
-       
         os.remove(temp_filename)
 
         return jsonify({'output': output})
@@ -44,4 +37,4 @@ def ejecutar_codigo():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
