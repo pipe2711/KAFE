e9817bb308af7fbbe4799ecc314b2c8e54c318c9
Karen Castaneda <karencastaneda301@gmail.com>
2025-06-04 10:41:50 -0500

    csv actualizacion

--- KAFE-SAAS/SERVER-FLASK/main.py
+++ KAFE-SAAS/SERVER-FLASK/main.py
@@ -36,5 +36,41 @@ def ejecutar_codigo():
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
+@app.route('/static/<path:filename>')
+def serve_svg(filename):
+    return send_from_directory(app.static_folder, filename)
+
+# Descargar archivo .txt según programa
+@app.route('/archivo_usuario/<programa>/<nombre>')
+def descargar_archivo_txt(programa, nombre):
+    base_dir = "/home/ubuntu/KAFE/KAFE-SAAS/SERVER-FLASK/archivos_usuario"
+    ruta = os.path.join(base_dir, programa)
+    return send_from_directory(ruta, nombre, as_attachment=True)
+
+@app.route('/subir_csv', methods=['POST'])
+def subir_csv():
+    if 'archivo' not in request.files:
+        return jsonify({'error': 'No se envió ningún archivo.'}), 400
+
+    archivo = request.files['archivo']
+    
+    if archivo.filename == '':
+        return jsonify({'error': 'El nombre del archivo está vacío.'}), 400
+
+    if not archivo.filename.endswith('.csv'):
+        return jsonify({'error': 'Solo se permiten archivos .csv'}), 400
+
+    try:
+        ruta_destino = '/home/ubuntu/KAFE/KAFE-SAAS/SERVER-FLASK/csv_usuario'
+        os.makedirs(ruta_destino, exist_ok=True)
+
+        ruta_final = os.path.join(ruta_destino, archivo.filename)
+        archivo.save(ruta_final)
+
+        return jsonify({'mensaje': f'Archivo {archivo.filename} subido correctamente.'}), 200
+
+    except Exception as e:
+        return jsonify({'error': f'Error al guardar el archivo: {str(e)}'}), 500
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
